ef84d3cd84f21d2befafe048617ac9df0a07dd2b
Alberto <albertovalidofuentes2001@gmail.com>
2022-10-06 10:13:28 +0100

    Añado la descripcion

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import Description from './components/Description'
+import Github from './components/GitHub';
 
 export default function App() {
   return (
