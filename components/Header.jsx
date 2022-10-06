758dd1879fb831551373d128ff27531cdcd837d7
Alberto <albertovalidofuentes2001@gmail.com>
2022-10-06 09:17:41 +0100

    Termino el Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Header() {
-  return (
-    <View style={styles.container}>
-      <Text>Header</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.direction}>
+                <View>
+                <Image
+                    source={require("../assets/avatar.png")}
+                    style={styles.img}
+                />
+                </View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.name}>Alberto Valido Fuentes</Text>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'grey',
-        height: 150,
-        alignItems: 'center',
+        height: 200,
         justifyContent: 'center',
+    },
+    direction: {
+        flexDirection: 'row'
+    },
+    img: {
+        width: 80,
+        height: 80,
+        marginLeft: 20
+    },
+    nameContainer: {
+        marginLeft: 20,
+        justifyContent: 'center'
+    },
+    name: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold'
     }
 })
