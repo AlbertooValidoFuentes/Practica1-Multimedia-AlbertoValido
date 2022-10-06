539fdc368a060480bf6f9083c808e6c9dd51582a
Alberto <albertovalidofuentes2001@gmail.com>
2022-10-06 09:27:20 +0100

    Creamos y importamos el componente Description

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import Description from './components/Description'
 
 export default function App() {
   return (
     <View>
       <Header />
+      <Description/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Description.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View} from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.container}>
+            <Text>Descripcion</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
